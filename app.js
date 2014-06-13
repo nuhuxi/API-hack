4d9fab0f6966dc1624df1d4795b5c8262874a31e
nsohail <nsohail92@gmail.com>
2014-06-12 23:54:47 -0400

    Scroll working

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+	$('#carousel_ul li:first').before($('#carousel_ul li:last')); 
+	$('.right-scroll').click(function(){
+		var item_width = $('#carousel_ul li').outerWidth() + 20; //get item width plus the margins
+		var left_indent = parseInt($('#carousel_ul').css('left')) + item_width; //cancel out the left attribute
+
+		$('#carousel_ul').animate({'left': left_indent},{queue:false, duration:500},function(){
+
+			$('#carousel_ul li:first').after($('#carousel_ul li:last'));
+			$('#carousel_ul').css({'left' : '-180px'});
+		}); 
+	});
+
+//when user clicks the image for sliding left  
+        $('.left-scroll').click(function(){  
+  
+            var item_width = $('#carousel_ul li').outerWidth() + 20;  
+  
+            /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */  
+            var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;  
+  
+            $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
+  
+            /* when sliding to left we are moving the last item before the first item */  
+            $('#carousel_ul li:first').before($('#carousel_ul li:last'));  
+  
+            /* and again, when we make that change we are setting the left indent of our unordered list to the default -210px */  
+            $('#carousel_ul').css({'left' : '-168px'});  
+            });  
+  
+        });  
+
+
+});
