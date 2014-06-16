d8026e800dc85cb59f070812f9e010243af6ba9a
nsohail <nsohail92@gmail.com>
2014-06-15 23:40:18 -0400

    started api

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+    google.maps.event.addDomListener(window, 'load', initialize);
+    
+    function getLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(initialize);
+        }
+        else{
+            alert('error');
+        }
+    }
+    
+    
+
+
+
+
 	$('#carousel_ul li:first').before($('#carousel_ul li:last')); 
 	$('.right-scroll').click(function(){
 		var item_width = $('#carousel_ul li').outerWidth() + 20; //get item width plus the margins
@@ -13,44 +29,64 @@ $(document).ready(function(){
 	});
 
 //when user clicks the image for sliding left  
-        $('.left-scroll').click(function(){  
+    $('.left-scroll').click(function(){  
   
-            var item_width = $('#carousel_ul li').outerWidth() + 20;  
+        var item_width = $('#carousel_ul li').outerWidth() + 20;  
   
-            /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */  
-            var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;  
+        /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */  
+        var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;  
   
-            $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
+        $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
   
             /* when sliding to left we are moving the last item before the first item */  
-            $('#carousel_ul li:last').before($('#carousel_ul li:first'));  
+        $('#carousel_ul li:last').before($('#carousel_ul li:first'));  
    
             //$('#carousel_ul').css({'left' : '0px'});  
-            });  
-  
         });  
+  
+    });  
 
 
 
+    
 
 
 
-$('.searchForm').submit(function(event){
-    event.preventDefault();
+    $('.searchForm').submit(function(event){
+        event.preventDefault();
+
+        var foodSearch = $(this).find("input[name='food']").val();
+        getFoodResults();
 
 
-    var foodSearch = $(this).find("input[name='food']").val();
-    getFoodResults();
-
-
-});
+    });
 
 
 
 
 });//document ends
 
+var map;
 
+function initialize(location){
+    
+    console.log(location);
+    //how the map should look
+    var mapOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(-34.397, 150.644)
+    };
+
+    map = new google.maps.Map(document.getElementById('map'),
+    mapOptions);
+}
+
+
+
+
+
+
+/*
 var getFoodResults = function(){
 
     var request = {
@@ -76,32 +112,10 @@ var getFoodResults = function(){
 
     });
 
-}; //getFoodResults ends
+};
 
 
-
-// var mapsUrl    = 'http://maps.googleapis.com/maps/api/directions/json' +
-//                  '?origin=Toronto&destination=Montreal&sensor=false';
-// var encodedUrl = encodeURIComponent(mapsUrl);
-// var proxyUrl   = 'http://jsonp.guffa.com/Proxy.ashx?url=' + encodedUrl;
-
-
-// $.ajax({
-//     url: proxyUrl,
-//     dataType: 'jsonp',
-//     cache: false,
-//     success: function (data) {
-//         console.log(data);
-//     }
-// });
-    // .done(function(){
-    //   console.log('result');
-    //     debugger;
-    //});
-
-    //};
-
-
+*/
 
 
 
