102633cd9775c2b8895a3385a9bb4e1e545bc12f
nsohail <nsohail92@gmail.com>
2014-06-18 14:39:31 -0400

    test

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ function callback(results, status){
             console.log(eachPhotoinArray);
 
             $('#carousel_ul').append("<li><div class='food-thumbnail'><img style='width=100%' src='"+eachPhotoinArray2+"'></div></li>");
-            $('.left-scroll').show();
+            $('.right-scroll').show();
         }
 
         var marker = new google.maps.Marker({ //marker for the search results
