59c8e50e98387f67c792a54829635a6579e38ed4
nsohail <nsohail92@gmail.com>
2014-06-19 13:55:43 -0400

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function(){
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+    //google.maps.event.addDomListener(window, 'load', initialize);
 
 
     getLocation();
