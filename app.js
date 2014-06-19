83b09d2415adfd22320e6c419128e76efa69956d
nsohail <nsohail92@gmail.com>
2014-06-19 12:15:54 -0400

    Slider change

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
         $('.left-scroll').show();
 
 		var item_width = $('#carousel_ul li').outerWidth() + 20; //store item width plus the margins
-		var left_indent = parseInt($('#carousel_ul').css('left')) + item_width; //cancel out the left attribute
+		var left_indent = parseInt($('#carousel_ul').css('left')) - item_width; //give left attribute
 
 		$('#carousel_ul').animate({'left': left_indent},{queue:false, duration:800},function(){
 
@@ -34,7 +34,7 @@ $(document).ready(function(){
         var item_width = $('#carousel_ul li').outerWidth() + 20;  
   
         /* same as for sliding right except that it's current left indent + the item width (for the sliding right it's - item_width) */  
-        var left_indent = parseInt($('#carousel_ul').css('left')) - item_width;  
+        var left_indent = parseInt($('#carousel_ul').css('left')) + item_width;  
   
         $('#carousel_ul').animate({'left' : left_indent},{queue:false, duration:500},function(){  
   
