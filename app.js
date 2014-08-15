65ae817bbb0ba07dcb9c0d0e30e0e6703de7e801
nsohail <nsohail92@gmail.com>
2014-08-15 14:27:53 -0400

    test

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ function callback(results, status){
         new google.maps.event.trigger(markersOnMap[pictureValue], 'click');
     });
 
-    $('.searchForm').find(".userChoice").val('');
+    //$('.searchForm').find("input[name='food']").val('');
 
 } //callback ends here
 
