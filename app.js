80f0bc8c25f62bf1992cc2b1867f3e57c8ef2dcf
nsohail <nsohail92@gmail.com>
2014-08-22 20:04:01 -0400

    test

--- app.js
+++ app.js
@@ -53,7 +53,6 @@ function placeDetailsCallback (place, status){
         var placeAddressObject = placeAddress[i];//each object in array of address objects
         console.log(placeAddressObject);
         //need to target object with a property types 'locality'
-        console.log(placeAddressObject.types[0]= "locality");
 
         if(placeAddressObject.types[i] == "locality"){
             alert(placeAddressObject.long_name);
