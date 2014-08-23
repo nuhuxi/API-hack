26c108a228a5ea832f6fb9f16fa602d79e81919c
nsohail <nsohail92@gmail.com>
2014-08-22 23:50:33 -0400

    test

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function placeDetailsCallback (place, status){
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     //console.log (place);
     var placeAddress = place.address_components;//an array of address objects
-    console.log(placeAddress);
+    //console.log(placeAddress);
 
     for (var i=0; i<placeAddress.length;i++){
         var placeAddressObject = placeAddress[i];//each object in array of address objects
@@ -55,6 +55,7 @@ function placeDetailsCallback (place, status){
 
         if(placeAddressObject.types[0] === "locality"){ //if the place address object has type property 'locality'
             console.log(placeAddressObject);
+            console.log(placeAddressObject.long_name);
         }
     }
 
