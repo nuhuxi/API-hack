3f2db70775fdb003f6263bf125a3af58a7f64c59
nsohail <nsohail92@gmail.com>
2014-08-24 00:13:35 -0400

    test

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ function callback(results, status){
         }
 
         else if (photoForPlace) {
-            var resultValue = $('.results').attr('value', i );
+            var resultValue = $('.results li').attr('value', i );
             $('.results').append("<li><div class='food-thumbnail'><img style='height:190px;min-width: 230px;' value = '"+i+"' src='"+photoForPlace+"'></div><div class='resultName'>"+placeName+"</div><div class='location'>"+cityArray[i]+"</div></li>");
         }
 
